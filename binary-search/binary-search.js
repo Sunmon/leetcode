4b297cc8f535483769948cf5173ac8838aa2325d
Sun-Jung Kim <blackmilk274@gmail.com>
2021-09-24 01:19:56 +0900

    Time: 76 ms (83.08%), Space: 42.3 MB (37.27%)

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,14 +2,19 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
+ * Time: 80 ms (69.52%), Space: 42.3 MB (37.27%) - LeetHub
  */
 var search = function(nums, target) {
-    // return nums.findIndex(el => el === target)
-  
+    return nums.findIndex(el => el === target)
+};
+
+/**
+ * Time: 76 ms (83.08%), Space: 42.3 MB (37.27%)
+ */
+var search = function(nums, target) {
   let [begin, end] = [0, nums.length-1];
-  
   while(begin <= end) {
-    const i = Number.parseInt((begin+end) / 2);
+    const i = ((begin+end) / 2)|0;
     if (target === nums[i]) return i;
     target > nums[i]? begin = i+1 : end = i-1;
   }
