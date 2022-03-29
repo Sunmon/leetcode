813ce48177926ca3f249593b04b3337fba6c1ad9
Sun-Jung Kim <blackmilk274@gmail.com>
2022-03-29 16:05:24 +0900

    Time: 113 ms (50.98%), Space: 50.8 MB (26.00%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -3,12 +3,32 @@
  * @return {number}
  */
 const maxSubArray = function (nums) {
-  let curSubArr = nums[0];
-  let maxSubArr = nums[0];
-  for (let i = 1; i < nums.length; i++) {
-    curSubArr = Math.max(nums[i], curSubArr + nums[i]);
-    maxSubArr = Math.max(maxSubArr, curSubArr);
+  return getMaxSub(nums, 0, nums.length);
+};
+
+const getMaxSub = function (nums, begin, end) {
+  if (begin >= end) {
+    return -987654321;
+  }
+  const half = Math.floor((begin + end) / 2);
+
+  let leftMid = 0;
+  let rightMid = 0;
+  let tempSum = 0;
+  for (let i = half - 1; i >= begin; i--) {
+    tempSum += nums[i];
+    leftMid = Math.max(tempSum, leftMid);
   }
 
-  return maxSubArr;
+  tempSum = 0;
+  for (let i = half + 1; i < end; i++) {
+    tempSum += nums[i];
+    rightMid = Math.max(tempSum, rightMid);
+  }
+
+  const mid = leftMid + nums[half] + rightMid;
+  const left = getMaxSub(nums, begin, half);
+  const right = getMaxSub(nums, half + 1, end);
+
+  return Math.max(...[mid, left, right]);
 };
