e2dbaf4ba89d60d7087a2fa9b1529d61cd2177fb
Sun-Jung Kim <blackmilk274@gmail.com>
2022-04-01 18:37:46 +0900

    Time: 136 ms (22.86%), Space: 46.1 MB (18.48%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.js
+++ 566-reshape-the-matrix/566-reshape-the-matrix.js
@@ -6,19 +6,14 @@
  */
 const matrixReshape = function (mat, r, c) {
   if (mat.length * mat[0].length !== r * c) return mat;
-  const queue = [];
+  const result = Array.from(Array(r), () => Array(c));
+  let k = 0;
   for (let i = 0; i < mat.length; i++) {
     for (let j = 0; j < mat[i].length; j++) {
-      queue.push(mat[i][j]);
+      result[parseInt(k / c)][k % c] = mat[i][j];
+      k++;
     }
   }
 
-  const result = Array.from(Array(r), () => Array(c));
-  let k = 0;
-  for (let i = 0; i < r; i++) {
-    for (let j = 0; j < c; j++) {
-      result[i][j] = queue[k++];
-    }
-  }
   return result;
 };
