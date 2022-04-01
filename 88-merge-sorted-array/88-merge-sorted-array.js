dd8bb6852adfcb1eac8a5629fcea439c1e227e60
Sun-Jung Kim <blackmilk274@gmail.com>
2022-04-01 11:44:13 +0900

    Time: 56 ms (97.35%), Space: 42 MB (70.14%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.js
+++ 88-merge-sorted-array/88-merge-sorted-array.js
@@ -8,16 +8,13 @@
 const merge = function (nums1, m, nums2, n) {
   let p1 = m - 1;
   let p2 = n - 1;
-  let p = m + n - 1;
-  while (p1 >= 0 || p2 >= 0) {
-    if (p1 < 0) {
-      nums1[p--] = nums2[p2--];
-    } else if (p2 < 0) {
-      nums1[p--] = nums1[p1--];
-    } else if (nums1[p1] <= nums2[p2]) {
-      nums1[p--] = nums2[p2--];
+
+  for (let p = m + n - 1; p >= 0; p--) {
+    if (p2 < 0) break;
+    if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+      nums1[p] = nums1[p1--];
     } else {
-      nums1[p--] = nums1[p1--];
+      nums1[p] = nums2[p2--];
     }
   }
 };
