78f3746ed543afcf8f8e245e9372dd4680779f13
Sun-Jung Kim <blackmilk274@gmail.com>
2022-04-01 16:54:06 +0900

    Time: 103 ms (31.88%), Space: 42.7 MB (73.79%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.js
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.js
@@ -4,17 +4,19 @@
  * @return {number[]}
  */
 const intersect = function (nums1, nums2) {
-  if (nums1.length < nums2.length) return intersect(nums2, nums1);
-  const m = new Map();
-  for (let i = 0; i < nums1.length; i++) {
-    m.set(nums1[i], m.get(nums1[i]) + 1 || 1);
-  }
+  nums1.sort((a, b) => a - b);
+  nums2.sort((a, b) => a - b);
 
-  let k = 0;
-  for (let i = 0; i < nums2.length; i++) {
-    if (m.get(nums2[i]) > 0) {
-      m.set(nums2[i], m.get(nums2[i]) - 1);
-      nums1[k++] = nums2[i];
+  let [i, j, k] = [0, 0, 0];
+
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] < nums2[j]) {
+      i++;
+    } else if (nums1[i] > nums2[j]) {
+      j++;
+    } else {
+      nums1[k++] = nums1[i++];
+      j++;
     }
   }
 
