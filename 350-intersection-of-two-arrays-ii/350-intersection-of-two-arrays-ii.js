4990e08e2016d202abee443a6f5967239d4695ed
Sun-Jung Kim <blackmilk274@gmail.com>
2022-04-01 12:49:58 +0900

    Time: 85 ms (59.67%), Space: 44.1 MB (43.08%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.js
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.js
@@ -4,28 +4,21 @@
  * @return {number[]}
  */
 const intersect = function (nums1, nums2) {
-  const m1 = new Map();
-  const m2 = new Map();
-  for (let i = 0; i < nums1.length; i++) {
-    if (m1.has(nums1[i])) {
-      m1.set(nums1[i], m1.get(nums1[i]) + 1);
+  nums1.sort((a, b) => a - b);
+  nums2.sort((a, b) => a - b);
+  const result = [];
+  let [p1, p2] = [0, 0];
+  while (p1 < nums1.length && p2 < nums2.length) {
+    if (nums1[p1] === nums2[p2]) {
+      result.push(nums1[p1]);
+      p1++;
+      p2++;
+    } else if (nums1[p1] > nums2[p2]) {
+      p2++;
     } else {
-      m1.set(nums1[i], 1);
-    }
-  }
-  for (let i = 0; i < nums2.length; i++) {
-    if (m2.has(nums2[i])) {
-      m2.set(nums2[i], m2.get(nums2[i]) + 1);
-    } else {
-      m2.set(nums2[i], 1);
+      p1++;
     }
   }
 
-  const result = [];
-  for (const [k, v] of m1) {
-    if (m2.has(k)) {
-      result.push(...Array(Math.min(v, m2.get(k))).fill(k));
-    }
-  }
   return result;
 };
