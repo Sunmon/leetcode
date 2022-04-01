dc323ed4dd2bd1bf038b69ecb2690839960b622a
Sun-Jung Kim <blackmilk274@gmail.com>
2022-04-01 11:41:15 +0900

    Time: 60 ms (93.34%), Space: 42.5 MB (23.12%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.js
+++ 88-merge-sorted-array/88-merge-sorted-array.js
@@ -6,9 +6,18 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 const merge = function (nums1, m, nums2, n) {
-  for (let i = 0; i < n; i++) {
-    nums1[m + i] = nums2[i];
+  let p1 = m - 1;
+  let p2 = n - 1;
+  let p = m + n - 1;
+  while (p1 >= 0 || p2 >= 0) {
+    if (p1 < 0) {
+      nums1[p--] = nums2[p2--];
+    } else if (p2 < 0) {
+      nums1[p--] = nums1[p1--];
+    } else if (nums1[p1] <= nums2[p2]) {
+      nums1[p--] = nums2[p2--];
+    } else {
+      nums1[p--] = nums1[p1--];
+    }
   }
-
-  nums1.sort((a, b) => a - b);
 };
