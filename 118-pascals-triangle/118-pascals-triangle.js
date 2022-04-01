856a3c9e6b9d537941cc8f83932413d00cb478f9
Sun-Jung Kim <blackmilk274@gmail.com>
2022-04-01 18:07:37 +0900

    Time: 63 ms (82.94%), Space: 42.2 MB (46.28%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.js
+++ 118-pascals-triangle/118-pascals-triangle.js
@@ -3,19 +3,18 @@
  * @return {number[][]}
  */
 const generate = function (numRows) {
-  const answer = [];
-  let temp = [];
-  for (let i = 0; i <= numRows; i++) {
-    for (let j = 0; j < i; j++) {
-      if (j === 0 || j === i - 1) {
+  const answer = [[1]];
+  for (let i = 1; i < numRows; i++) {
+    const temp = [];
+    for (let j = 0; j <= i; j++) {
+      if (j === 0 || j === i) {
         temp.push(1);
       } else {
-        temp.push(answer[i - 1][j - 1] + answer[i - 1][j]);
+        temp.push(answer[i - 1][j] + answer[i - 1][j - 1]);
       }
     }
     answer.push(temp);
-    temp = [];
   }
-  answer.shift();
   return answer;
 };
+
