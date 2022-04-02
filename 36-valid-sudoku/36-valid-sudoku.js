e03e8579e5ce74a4633c41d9755807ca67dd7925
Sun-Jung Kim <blackmilk274@gmail.com>
2022-04-02 18:13:18 +0900

    Time: 121 ms (41.67%), Space: 45.3 MB (63.98%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.js
+++ 36-valid-sudoku/36-valid-sudoku.js
@@ -1,21 +1,29 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
 const isValidSudoku = function (board) {
-  const rows = Array.from(Array(9), () => new Set());
-  const cols = Array.from(Array(9), () => new Set());
-  const cells = Array.from(Array(9), () => new Set());
+  const N = 9;
+  const rows = Array(9);
+  const cols = Array(9);
+  const boxes = Array(9);
 
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
       if (board[i][j] === '.') continue;
-      const num = +board[i][j] - 1;
-        const sector = parseInt(i/3) * 3 + parseInt(j/3);
-      if (rows[i].has(num)) return false;
-      if (cols[j].has(num)) return false;
-      if (cells[sector].has(num)) return false;
-      rows[i].add(num);
-      cols[j].add(num);
-      cells[sector].add(num);
+      const val = +board[i][j];
+      const pos = 1 << (val - 1); // pos 번째 비트 확인용
+
+      // 비트마스킹을 이용하여 숫자 중복 여부 확인
+      if ((rows[i] & pos) > 0) return false;
+      if ((cols[j] & pos) > 0) return false;
+      if ((boxes[parseInt(i / 3) * 3 + parseInt(j / 3)] & pos) > 0)
+        return false;
+
+      rows[i] |= pos;
+      cols[j] |= pos;
+      boxes[parseInt(i / 3) * 3 + parseInt(j / 3)] |= pos;
     }
   }
-
   return true;
 };
