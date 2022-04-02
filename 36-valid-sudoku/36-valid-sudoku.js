a3afbb7398c1fb783af2edf18d15d083ce5c7737
Sun-Jung Kim <blackmilk274@gmail.com>
2022-04-02 17:04:01 +0900

    Time: 111 ms (53.24%), Space: 46.7 MB (34.51%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.js
+++ 36-valid-sudoku/36-valid-sudoku.js
@@ -1,7 +1,3 @@
-/**
- * @param {character[][]} board
- * @return {boolean}
- */
 const isValidSudoku = function (board) {
   const rows = Array.from(Array(9), () => new Set());
   const cols = Array.from(Array(9), () => new Set());
@@ -9,14 +5,15 @@ const isValidSudoku = function (board) {
 
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
-      if (isNaN(board[i][j])) continue;
+      if (board[i][j] === '.') continue;
       const num = +board[i][j] - 1;
+        const sector = parseInt(i/3) * 3 + parseInt(j/3);
       if (rows[i].has(num)) return false;
       if (cols[j].has(num)) return false;
-      if (cells[parseInt(i / 3) * 3 + parseInt(j / 3)].has(num)) return false;
+      if (cells[sector].has(num)) return false;
       rows[i].add(num);
       cols[j].add(num);
-      cells[parseInt(i / 3) * 3 + parseInt(j / 3)].add(num);
+      cells[sector].add(num);
     }
   }
 
