dcffc1dea1c1788fd58d6133d52e689fe4a48d45
Sun-Jung Kim <blackmilk274@gmail.com>
2022-04-12 22:50:37 +0900

    Time: 83 ms (44.83%), Space: 42.1 MB (67.63%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -4,12 +4,12 @@
  * @return {number}
  */
 const searchInsert = function (nums, target) {
-  let [begin, end] = [0, nums.length - 1];
+  let [begin, end] = [0, nums.length-1];
   while (begin <= end) {
     const middle = begin + parseInt((end - begin) / 2);
     if (nums[middle] === target) return middle;
     if (nums[middle] > target) {
-      end = middle - 1;
+      end = middle-1;
     } else {
       begin = middle + 1;
     }
