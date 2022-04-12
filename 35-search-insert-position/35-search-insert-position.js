1538875a8ec7068f86a0d2dee2aa995c6e9cf13b
Sun-Jung Kim <blackmilk274@gmail.com>
2022-04-12 22:49:43 +0900

    Time: 56 ms (96.84%), Space: 42.8 MB (9.90%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -4,18 +4,15 @@
  * @return {number}
  */
 const searchInsert = function (nums, target) {
-  return binarySearch(nums, target, 0, nums.length);
-};
-
-// search nums[begin,end)
-const binarySearch = function (nums, target, begin, end) {
-  if (begin >= end) {
-    return begin;
+  let [begin, end] = [0, nums.length - 1];
+  while (begin <= end) {
+    const middle = begin + parseInt((end - begin) / 2);
+    if (nums[middle] === target) return middle;
+    if (nums[middle] > target) {
+      end = middle - 1;
+    } else {
+      begin = middle + 1;
+    }
   }
-  const middle = parseInt((begin + end) / 2, 10);
-  if (nums[middle] === target) return middle;
-  if (nums[middle] > target) {
-    return binarySearch(nums, target, begin, middle);
-  }
-  return binarySearch(nums, target, middle + 1, end);
+  return begin;
 };
