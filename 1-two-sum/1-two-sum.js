e2dc695d677a936ad87a828aeedf7a4807458944
Sun-Jung Kim <blackmilk274@gmail.com>
2022-04-13 19:51:46 +0900

    Time: 75 ms (85.85%), Space: 43.3 MB (25.49%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,13 +4,12 @@
  * @return {number[]}
  */
 const twoSum = function (nums, target) {
-  const cache = new Map();
+  const s = new Map();
   for (let i = 0; i < nums.length; i++) {
-    if (cache.has(target - nums[i])) {
-      return [cache.get(target - nums[i]), i];
+    if (s.has(target - nums[i])) {
+      return [i, s.get(target - nums[i])];
     }
-    cache.set(nums[i], i);
+    s.set(nums[i], i);
   }
-
-  return [-1, -1];
+  return [];
 };
