f3dce9dba1053dbd6dcd88364a62830bff58cdfa
Sun-Jung Kim <blackmilk274@gmail.com>
2022-04-15 17:34:56 +0900

    Time: 101 ms (67.18%), Space: 51.9 MB (33.43%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -4,21 +4,13 @@
  */
 const maxProfit = function (prices) {
   let answer = 0;
-  const len = prices.length;
-  const lowestPrices = Array(len);
-  const highestPrices = Array(len);
-  let low = 987654321;
-  let high = -987654321;
-  for (let i = 0; i < len; i++) {
-    if (low > prices[i]) low = prices[i];
-    if (high < prices[len - i - 1]) high = prices[len - i - 1];
-    lowestPrices[i] = low;
-    highestPrices[len - i - 1] = high;
+  let minPrice = 987654321;
+  for (let i = 0; i < prices.length; i++) {
+    if (minPrice > prices[i]) {
+      minPrice = prices[i];
+    } else {
+      answer = Math.max(answer, prices[i] - minPrice);
+    }
   }
-
-  for (let i = 0; i < len; i++) {
-    answer = Math.max(answer, highestPrices[i] - lowestPrices[i]);
-  }
-
   return answer;
 };
