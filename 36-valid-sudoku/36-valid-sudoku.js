152d34ef5ba1ec7382f8d5373c824a9556f8530a
Sun-Jung Kim <blackmilk274@gmail.com>
2022-04-16 15:39:44 +0900

    Time: 83 ms (83.85%), Space: 46.9 MB (31.86%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.js
+++ 36-valid-sudoku/36-valid-sudoku.js
@@ -3,27 +3,23 @@
  * @return {boolean}
  */
 const isValidSudoku = function (board) {
-  const N = 9;
-  const rows = Array(9);
-  const cols = Array(9);
-  const boxes = Array(9);
+  const LEN = 9;
+  const rows = Array.from(Array(LEN), () => Array(LEN).fill(false));
+  const cols = Array.from(Array(LEN), () => Array(LEN).fill(false));
+  const blocks = Array.from(Array(LEN), () => Array(LEN).fill(false));
 
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j < N; j++) {
-      if (board[i][j] === '.') continue;
-      const val = +board[i][j];
-      const pos = 1 << (val - 1); // pos 번째 비트 확인용
-
-      // 비트마스킹을 이용하여 숫자 중복 여부 확인
-      if ((rows[i] & pos) > 0) return false;
-      if ((cols[j] & pos) > 0) return false;
-      if ((boxes[parseInt(i / 3) * 3 + parseInt(j / 3)] & pos) > 0)
-        return false;
-
-      rows[i] |= pos;
-      cols[j] |= pos;
-      boxes[parseInt(i / 3) * 3 + parseInt(j / 3)] |= pos;
+  for (let i = 0; i < LEN; i++) {
+    for (let j = 0; j < LEN; j++) {
+      const val = board[i][j];
+      if (val === '.') continue;
+      if (rows[i][+val]) return false;
+      if (cols[j][+val]) return false;
+      if (blocks[parseInt(i / 3) * 3 + parseInt(j / 3)][+val]) return false;
+      rows[i][+val] = true;
+      cols[j][+val] = true;
+      blocks[parseInt(i / 3) * 3 + parseInt(j / 3)][+val] = true;
     }
   }
+
   return true;
 };
