40cc9b2f744552abc957376f221aaef78d4adec9
Sun-Jung Kim <blackmilk274@gmail.com>
2022-04-16 15:53:52 +0900

    Time: 60 ms (92.44%), Space: 42.3 MB (44.32%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -2,29 +2,22 @@
  * @param {number[][]} matrix
  * @param {number} target
  * @return {boolean}
-
  */
 const searchMatrix = function (matrix, target) {
-  const row = binaryRowSearch(matrix, target, 0, matrix.length);
-  const col = binaryColSearch(matrix[row], target, 0, matrix[row].length);
-  return matrix[row][col] === target;
-};
+  const [m, n] = [matrix.length, matrix[0].length];
+  let begin = 0;
+  let end = m * n;
+  while (begin < end) {
+    const p = parseInt((begin + end) / 2);
+    const [i, j] = [parseInt(p / n), p % n];
 
-// [begin, end) 탐색
-const binaryRowSearch = function (matrix, target, begin, end) {
-  if (begin >= end - 1) return begin;
-  const mid = parseInt((begin + end) / 2);
-  if (matrix[mid][0] < target) return binaryRowSearch(matrix, target, mid, end);
-  if (matrix[mid][0] > target)
-    return binaryRowSearch(matrix, target, begin, mid);
-  return mid;
-};
+    if (matrix[i][j] === target) return true;
+    if (matrix[i][j] > target) {
+      end = p;
+    } else {
+      begin = p + 1;
+    }
+  }
 
-const binaryColSearch = function (matrix, target, begin, end) {
-  if (begin >= end - 1) return begin;
-  const mid = parseInt((begin + end) / 2);
-  if (matrix[mid] < target)
-    return binaryColSearch(matrix, target, mid + 1, end);
-  if (matrix[mid] > target) return binaryColSearch(matrix, target, begin, mid);
-  return mid;
+  return false;
 };
