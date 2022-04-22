913a16e07eb5e75cab39d334ccad4269af71d242
Sun-Jung Kim <blackmilk274@gmail.com>
2022-04-22 19:26:58 +0900

    Time: 84 ms (39.04%), Space: 41.8 MB (92.15%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.js
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.js
@@ -11,24 +11,14 @@
  * @return {number[]}
  */
 const preorderTraversal = function (root) {
-  const route = [];
-  const stack = [];
-  if (root === null) return [];
+  const answer = [];
+  traversal(root, answer);
+  return answer;
+};
 
-  stack.push(root);
-  let cur = root;
-  // route.push(cur.val);
-  while (stack.length > 0) {
-    route.push(cur.val);
-    if (cur?.right) {
-      stack.push(cur.right);
-    }
-
-    if (cur?.left) {
-      stack.push(cur.left);
-    }
-
-    cur = stack.pop();
-  }
-  return route;
+const traversal = function (node, arr) {
+  if (!node) return;
+  arr.push(node.val);
+  traversal(node.left, arr);
+  traversal(node.right, arr);
 };
