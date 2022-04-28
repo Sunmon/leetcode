ce9dd2cc38f888cf51b2c5dbc21488e2d77427ec
Sun-Jung Kim <blackmilk274@gmail.com>
2022-04-28 23:34:36 +0900

    Time: 1035 ms (8.96%), Space: 0B (100.00%) - LeetHub

---
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT user_id,
+    CONCAT (
+        upper(substring(name, 1, 1)),
+        lower(substring(name, 2))
+    ) as name
+FROM Users
+ORDER BY user_id;
