3d208c3d4cb7ffe0d9f8bffe5b4ce1f61d1393af
Sun-Jung Kim <blackmilk274@gmail.com>
2022-04-28 23:04:19 +0900

    Time: 144 ms (29.56%), Space: 54.8 MB (18.43%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -6,7 +6,6 @@
 let parent = [];
 let rank = [];
 let count = 0;
-// let visited = [[]];
 const dir = [
   [-1, 0],
   [1, 0],
@@ -16,18 +15,15 @@ const dir = [
 const numIslands = function (grid) {
   const [m, n] = [grid.length, grid[0].length];
   count = 0;
-
-  // parent = Array.from(Array(m), () => Array(n).fill([-1, -1]));
   parent = [...Array(m * n)].map((v, i) => {
     if (grid[parseInt(i / n)][i % n] === '1') ++count;
     return i;
   });
   rank = Array(m * n).fill(1);
-  // visited = Array.from(Array(m), () => Array(n).fill(false));
+
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
       if (grid[i][j] === '1') {
-        // grid[i][j] = 0;
         for (let k = 0; k < 4; k++) {
           const [nx, ny] = [i + dir[k][0], j + dir[k][1]];
           if (grid[nx]?.[ny] === '1') union(i * n + j, nx * n + ny);
