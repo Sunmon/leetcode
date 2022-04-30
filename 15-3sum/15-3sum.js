48281fd85dd9efba6d561433d988e07252336a49
Sun-Jung Kim <blackmilk274@gmail.com>
2022-04-30 11:39:32 +0900

    Time: 497 ms (22.61%), Space: 59.6 MB (17.65%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -8,26 +8,23 @@ const threeSum = function (nums) {
   const answer = [];
   for (let pivot = 0; pivot < nums.length; pivot++) {
     if (pivot === 0 || nums[pivot - 1] !== nums[pivot]) {
-      twoSumII(pivot, nums, answer);
+      twoSum(pivot, nums, answer);
     }
   }
   return answer;
 };
 
-const twoSumII = function (pivot, nums, answer) {
-  let left = pivot + 1;
-  let right = nums.length - 1;
-  while (left < right) {
-    const sum = nums[pivot] + nums[left] + nums[right];
-    if (sum === 0) {
-      answer.push([nums[pivot], nums[left++], nums[right--]]);
-      while (left < right && nums[left] === nums[left - 1]) {
-        ++left;
-      }
-    } else if (sum > 0) {
-      --right;
+const twoSum = function (pivot, nums, answer) {
+  const seen = new Set();
+  for (let i = pivot + 1; i < nums.length; i++) {
+    const sum = nums[pivot] + nums[i];
+    if (seen.has(-sum)) {
+      answer.push([nums[pivot], nums[i], -sum]);
+      while (nums[i] === nums[i + 1]) i++;
     } else {
-      ++left;
+      seen.add(nums[i]);
+      // while (nums[i] === nums[i + 1]) i++;
+      // 여기 넣으면 연속된 숫자가 있을때, 기껏 seen에 넣은 nums[i]를 이용하지 않음
     }
   }
 };
