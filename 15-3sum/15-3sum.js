351c621f2c26e8327054d48f0fc855d214f82e8e
Sun-Jung Kim <blackmilk274@gmail.com>
2022-04-30 11:39:51 +0900

    Time: 4364 ms (6.08%), Space: 61.5 MB (14.93%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -4,27 +4,22 @@
  */
 
 const threeSum = function (nums) {
-  nums = nums.sort((a, b) => a - b);
-  const answer = [];
+  const duplicates = new Set();
+  const answer = new Set();
+  let seen = new Set();
   for (let pivot = 0; pivot < nums.length; pivot++) {
-    if (pivot === 0 || nums[pivot - 1] !== nums[pivot]) {
-      twoSum(pivot, nums, answer);
-    }
-  }
-  return answer;
-};
-
-const twoSum = function (pivot, nums, answer) {
-  const seen = new Set();
-  for (let i = pivot + 1; i < nums.length; i++) {
-    const sum = nums[pivot] + nums[i];
-    if (seen.has(-sum)) {
-      answer.push([nums[pivot], nums[i], -sum]);
-      while (nums[i] === nums[i + 1]) i++;
-    } else {
+    if (duplicates.has(pivot)) continue;
+    seen = new Set();
+    for (let i = pivot + 1; i < nums.length; i++) {
+      const sum = nums[pivot] + nums[i];
+      if (seen.has(-sum)) {
+        // NOTE: in js, [1,2,3] !== [1,2,3]
+        const result = [nums[pivot], nums[i], -sum].sort((a, b) => a - b);
+        answer.add(JSON.stringify(result));
+      }
       seen.add(nums[i]);
-      // while (nums[i] === nums[i + 1]) i++;
-      // 여기 넣으면 연속된 숫자가 있을때, 기껏 seen에 넣은 nums[i]를 이용하지 않음
     }
+    duplicates.add(pivot);
   }
+  return [...answer.values()].map(i => JSON.parse(i));
 };
