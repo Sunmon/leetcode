2c2f57169f3add379120d622f5d1cda62b8553cc
Sun-Jung Kim <blackmilk274@gmail.com>
2022-05-05 18:51:38 +0900

    Time: 1174 ms (52.63%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -1,5 +1,6 @@
-select user_id as buyer_id, join_date, ifnull(orders_in_2019, 0) as orders_in_2019
-from users left join (select buyer_id, count(order_id) as orders_in_2019
-from orders
-where YEAR(order_date) = 2019
-group by buyer_id) as o on user_id = o.buyer_id;
+SELECT u.user_id AS buyer_id, join_date, COUNT(order_date) AS orders_in_2019 
+FROM Users as u
+LEFT JOIN Orders as o
+ON u.user_id = o.buyer_id
+AND YEAR(order_date) = '2019'
+GROUP BY u.user_id
