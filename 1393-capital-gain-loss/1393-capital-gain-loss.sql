ba98ce73cf831f8b16b1ababeba0b9f5caf41089
Sun-Jung Kim <blackmilk274@gmail.com>
2022-05-05 18:31:09 +0900

    Time: 479 ms (70.17%), Space: 0B (100.00%) - LeetHub

--- 1393-capital-gain-loss/1393-capital-gain-loss.sql
+++ 1393-capital-gain-loss/1393-capital-gain-loss.sql
@@ -1,13 +1,8 @@
-
-select stock_name, (sell.price - buy.price) as capital_gain_loss
-from 
-(select stock_name, sum(price) as price
-from stocks
-where operation = 'Buy'
-group by stock_name) as buy
-join 
-(select stock_name, sum(price) as price
-from stocks
-where operation = 'Sell'
-group by stock_name) as sell
-using (stock_name);
+SELECT stock_name, SUM(
+    CASE
+        WHEN operation = 'Buy' THEN -price
+        ELSE price
+    END
+) AS capital_gain_loss
+FROM Stocks
+GROUP BY stock_name
