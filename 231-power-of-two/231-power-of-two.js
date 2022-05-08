650d3e0a6852c64f34bf4891a2a966fccc782a89
Sun-Jung Kim <blackmilk274@gmail.com>
2022-05-08 19:36:07 +0900

    Time: 126 ms (13.54%), Space: 43.3 MB (79.95%) - LeetHub

--- 231-power-of-two/231-power-of-two.js
+++ 231-power-of-two/231-power-of-two.js
@@ -3,11 +3,5 @@
  * @return {boolean}
  */
 const isPowerOfTwo = function (n) {
-  let counter = 0;
-  while (n > 0 && counter < 2) {
-    if ((1 & n) !== 0) counter++;
-    n >>= 1;
-  }
-
-  return counter === 1;
+    return Math.log2(n) %1 === 0;
 };
