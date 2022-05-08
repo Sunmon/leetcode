222f4a3c335a64e829b49d1536c5033b27a77ad3
Sun-Jung Kim <blackmilk274@gmail.com>
2022-05-08 19:44:26 +0900

    Time: 137 ms (6.12%), Space: 42.1 MB (94.75%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.js
+++ 191-number-of-1-bits/191-number-of-1-bits.js
@@ -4,9 +4,9 @@
  */
 var hammingWeight = function(n) {
     let counter = 0;
-    for(let i = 0; i<32; i++) {
-        if (n&1) counter++;
-        n >>= 1;
+    while(n !== 0) {
+        counter++;
+        n &= (n-1);
     }
     return counter;
 };
