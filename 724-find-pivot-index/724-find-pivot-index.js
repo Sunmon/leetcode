30de56ded5e53ffaa8b4b98aeed6c4b09623db45
Sun-Jung Kim <blackmilk274@gmail.com>
2022-07-19 11:32:05 +0900

    Time: 103 ms (71.80%), Space: 44.2 MB (88.06%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.js
+++ 724-find-pivot-index/724-find-pivot-index.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var pivotIndex = function(nums) {
-  let sum = 0, leftSum = 0;
-  sum = nums.reduce((prev, cur) => prev + cur);
-  for(let i = 0; i<nums.length; i++){
-    if (leftSum === sum - leftSum - nums[i]) return i;
-    leftSum += nums[i];
+  let sum = nums.reduce((prev, cur) => prev + cur);
+  let subSum = 0;
+  for(let i = 0 ;i<nums.length; i++) {
+    if (sum - subSum - nums[i] === subSum) return i;
+    subSum += nums[i];
   }
   return -1;
 };
