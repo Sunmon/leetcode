dc06df4623eed0a4a27c1338dec71fd20b6eae7b
Sun-Jung Kim <blackmilk274@gmail.com>
2022-09-14 23:11:30 +0900

    Time: 124 ms (19.47%), Space: 44.3 MB (29.85%) - LeetHub

---
+++ 190-reverse-bits/190-reverse-bits.js
@@ -0,0 +1,13 @@
+var reverseBits = function(n) {
+    let res = 0, pow = 31;
+    while (n > 0) {
+        let rightMost = n & 1;
+        res = res + (rightMost << pow);
+        pow--;
+        n = n >>> 1;
+    }
+    // take negative into positive
+    return res >>> 0;
+    // T.C: O(1)
+    // S.C: O(1)
+};
