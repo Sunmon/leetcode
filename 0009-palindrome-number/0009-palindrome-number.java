168831de8e3f5488691041f2d2814088973f9446
Sun-Jung Kim <blackmilk274@gmail.com>
2024-08-19 00:11:33 +0900

    Time: 6 ms (31.58%), Space: 43.7 MB (74.99%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,7 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
         String str1 = Integer.toString(x);
-        String str2 = new StringBuffer(str1).reverse().toString();
-        return str1.equals(str2);
+        // String str2 = new StringBuffer(str1).reverse().toString();
+        // return str1.equals(str2);
+        for(int i = 0; i<str1.length(); i++){
+            if (str1.charAt(i) != str1.charAt(str1.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+        
     }
 }
